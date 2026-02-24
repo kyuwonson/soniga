16cef8759bebe9e0359f9a2953d434c4d66ec960
Kyuwon Son <kyuwon0916@ajou.ac.kr>
2026-02-24 19:22:19 +0900

    배포문제 fix(26.2.24)

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   // GitHub Pages 배포를 위한 설정 (빌드 시에만 적용)
   ...(process.env.NODE_ENV === 'production' && {
     output: 'export',
+    basePath: '/soniga',
+    assetPrefix: '/soniga/',
     images: {
       unoptimized: true,
     },
